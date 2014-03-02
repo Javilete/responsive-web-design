fb0f24b1b82cf554a7a705d41a86b03aceae83e9
Javier Rubio <javilete14@gmail.com>
2014-03-02 20:18:09 +0000

    Updates according to feedback

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 $(function(){
-    $(document).ready(function(){
-        $('#linkHide').click(function(){
-            $('#promo').fadeOut('slow');
-        });
-        
-        $('#promo p a').click(function(e){
-            e.preventDefault();
-            $('#promo p').text('Provide the promotion code "W3C_Rules" during your next subscription');
-        });
-      
-      });    
+    $('#main div#promo').append('<a id="linkHide" href="#">Hide Promotion</a>');
+    
+    $('#linkHide').click(function(){
+        $('#promo').fadeOut('slow');
+    });
+
+    $('#promo p a').click(function(e){
+        e.preventDefault();
+        $('#promo p').text('Provide the promotion code "W3C_Rules" during your next subscription');
+    });
+
 });
 
